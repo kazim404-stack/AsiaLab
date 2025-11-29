7f106612356344ee2918920b52a44e69650daa17
kazim404-stack <kazimmohammadi404@gmail.com>
2025-11-29 09:13:09 +0430

    google site verification added to the front_master

--- resources/views/frontend/layouts/front_master.blade.php
+++ resources/views/frontend/layouts/front_master.blade.php
@@ -12,6 +12,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="google-site-verification" content="I-MaQSF-htVobr5642B-ESD8EpAh1jcVN4bI1zhcKJM" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <!-- Meta Description -->
